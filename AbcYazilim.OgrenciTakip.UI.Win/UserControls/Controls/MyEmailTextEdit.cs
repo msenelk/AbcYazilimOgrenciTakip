51f7d2e5c7cae19c53e0de1da232d2c3e1803c07
Muhammed Kizilirmak <senel_1996@hotmail.com>
2024-10-23 21:31:45 +0300

    Email bileşeni oluşturuldu.

---
+++ AbcYazilim.OgrenciTakip.UI.Win/UserControls/Controls/MyEmailTextEdit.cs
@@ -0,0 +1,18 @@
+﻿using AbcYazilim.OgrenciTakip.UI.Win.Interfaces;
+using DevExpress.XtraEditors.Mask;
+using System.ComponentModel;
+
+namespace AbcYazilim.OgrenciTakip.UI.Win.UserControls.Controls
+{
+    internal class MyEmailTextEdit : MyTextEdit
+    {
+        [ToolboxItem(true)]
+        public MyEmailTextEdit()
+        {
+            Properties.Mask.MaskType = MaskType.RegEx;
+            Properties.Mask.EditMask = "((([0-9a-zA-Z_%-])+[.])+|([0-9a-zA-Z_%-])+)+@((([0-9a-zA-Z_-])+[.])+|([0-9a-zA-Z_-])+)+";
+            Properties.Mask.AutoComplete = AutoCompleteType.Strong;
+            StatusBarAciklama = "E-Mail Adresi Giriniz.";
+        }
+    }
+}
