05f6111cfbe27ee2b2e877024a6a580e652a1ae8
Muhammed Kizilirmak <senel_1996@hotmail.com>
2024-10-25 22:20:18 +0300

    Grid control eklendi.

---
+++ AbcYazilim.OgrenciTakip.UI.Win/UserControls/Grid/MyGridControl.cs
@@ -0,0 +1,132 @@
+﻿using AbcYazilim.OgrenciTakip.UI.Win.Interfaces;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.Utils;
+using DevExpress.XtraEditors.Mask;
+using DevExpress.XtraGrid.Views.Base;
+using System.Drawing;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraGrid.Registrator;
+using System.ComponentModel;
+
+namespace AbcYazilim.OgrenciTakip.UI.Win.UserControls.Grid
+{
+    [ToolboxItem(true)]
+    public class MyGridControl:GridControl
+    {
+        protected override BaseView CreateDefaultView()
+        {
+            var view = (GridView)CreateView("MyGridView");
+            view.Appearance.ViewCaption.ForeColor = Color.Maroon;
+            view.Appearance.HeaderPanel.ForeColor = Color.Maroon;
+            view.Appearance.HeaderPanel.TextOptions.HAlignment = HorzAlignment.Center;
+           view.Appearance.FooterPanel.ForeColor = Color.Maroon;
+            view.Appearance.FooterPanel.Font = new Font(new FontFamily("Tahoma"),8.25f,FontStyle.Bold);
+
+            // Sağ tık menü gizleme için
+            view.OptionsMenu.EnableColumnMenu = false;
+            view.OptionsMenu.EnableFooterMenu = false;
+            view.OptionsMenu.EnableGroupPanelMenu = false;
+
+            //enter tuşu ile dolaşım için
+            view.OptionsNavigation.EnterMoveNextColumn = true;
+
+            // yazıcıya gönderim için genişliği otomatik ayarlamasını iptal ettik
+            view.OptionsPrint.AutoWidth = false;
+            // footer bölümlerinin yazıcıya gönderilmesini istemiyoruz
+            view.OptionsPrint.PrintFooter = false;
+            
+            view.OptionsPrint.PrintGroupFooter = false;
+
+            view.OptionsView.ShowViewCaption = true;
+            view.OptionsView.ShowAutoFilterRow = true;
+            view.OptionsView.ShowGroupPanel = false;
+            // kolonlarımız belirlediğimiz genişlikte kalması için
+            view.OptionsView.ColumnAutoWidth = false;
+            // birden fazla satırlı yazıyı görebilmek için
+            view.OptionsView.RowAutoHeight = true;
+            view.OptionsView.HeaderFilterButtonShowMode = FilterButtonShowMode.Button;
+
+            var idColumn = new MyGridColumn();
+            // Caption = Kolon Başlığı
+            idColumn.Caption = "Id";
+            // FieldName = Kolon Adı
+            idColumn.FieldName = "Id";
+            idColumn.OptionsColumn.AllowEdit = false;
+            idColumn.OptionsColumn.ShowInCustomizationForm = false;
+            view.Columns.Add(idColumn);
+
+            // daha sade yazımı
+            var kodColumn = new MyGridColumn
+            {
+                Caption = "Kod",
+                FieldName = "Kod"
+            };
+            kodColumn.OptionsColumn.AllowEdit = false;
+            kodColumn.Visible = true;
+            // Başlıkları ortala
+            kodColumn.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+            // Başlıkları ortalamak için bunun kullanılmasını söylememiz gerekiyor
+            kodColumn.AppearanceCell.Options.UseTextOptions= true;
+            view.Columns.Add(kodColumn);
+
+            return view;
+        }
+        protected override void RegisterAvailableViewsCore(InfoCollection collection)
+        {
+            base.RegisterAvailableViewsCore(collection);
+            collection.Add(new MyGridInfoRegistrator());
+        }
+        private class MyGridInfoRegistrator : GridInfoRegistrator
+        {
+            public override string ViewName => "MyGridView";
+            public override BaseView CreateView(GridControl grid) => new MyGridView(grid);
+        }
+    }
+    public class MyGridView : GridView, IStatusBarKisaYol
+    {
+        public MyGridView() { }
+        public MyGridView(GridControl ownerGrid) : base(ownerGrid) { }
+        #region Properties
+        public string StatusBarKisaYol { get; set; }
+        public string StatusBarKisaYolAciklama { get; set; }
+        public string StatusBarAciklama { get; set; }
+        #endregion // Properties
+        protected override void OnColumnChangedCore(GridColumn column)
+        {
+            base.OnColumnChangedCore(column);
+
+            if (column.ColumnEdit == null) return;
+            if (column.ColumnEdit.GetType() == typeof(RepositoryItemDateEdit))
+            {
+                column.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+                ((RepositoryItemDateEdit)column.ColumnEdit).Mask.MaskType = MaskType.DateTimeAdvancingCaret;
+            }
+        }
+        protected override GridColumnCollection CreateColumnCollection()
+        {
+            return new MyGridColumnCollection(this);
+        }
+        private class MyGridColumnCollection : GridColumnCollection
+        {
+            public MyGridColumnCollection(ColumnView view) : base(view) { }
+            protected override GridColumn CreateColumn()
+            {
+                // kolon oluşturuyoruz
+                var column = new MyGridColumn();
+                column.OptionsColumn.AllowEdit = false; // Düzenlemeye izin vermiyoruz.
+                return column;
+            }
+        }
+    }
+    public class MyGridColumn : GridColumn,IStatusBarKisaYol
+    {
+      #region Properties
+        public string StatusBarKisaYol { get; set; }
+        public string StatusBarKisaYolAciklama { get; set; }
+        public string StatusBarAciklama { get; set; }
+        #endregion // Properties
+    }
+}
